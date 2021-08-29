a69065cdd10f95f81f6e964fd91fc6cf5f274e4b
Sahil <sohil170246@gmail.com>
2021-08-29 22:47:27 +0530

    'changes'

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
-    "plugin:prettier/recommended",
+    
   ],
 };
 
--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import { Switch, Route, Redirect } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 // creates a beautiful scrollbar
 import PerfectScrollbar from "perfect-scrollbar";
 import "perfect-scrollbar/css/perfect-scrollbar.css";
@@ -7,40 +7,37 @@ import "perfect-scrollbar/css/perfect-scrollbar.css";
 import { makeStyles } from "@material-ui/core/styles";
 // core components
 import Navbar from "components/Navbars/Navbar.js";
-// import Footer from "components/Footer/Footer.js";
+import Footer from "components/Footer/Footer.js";
 import Sidebar from "components/Sidebar/Sidebar.js";
 import FixedPlugin from "components/FixedPlugin/FixedPlugin.js";
-
 import routes from "routes.js";
-
 import styles from "assets/jss/material-dashboard-react/layouts/adminStyle.js";
-
 import bgImage from "assets/img/sidebar-2.jpg";
 import logo from "assets/img/reactlogo.png";
 
 let ps;
 
-// const switchRoutes = (
-//   <Switch>
-//     {routes.map((prop, key) => {
-//       if (prop.layout === "/admin") {
-//         return (
-//           <Route
-//             path={prop.layout + prop.path}
-//             component={prop.component}
-//             key={key}
-//           />
-//         );
-//       }
-//       return null;
-//     })}
-//     <Redirect from="/admin" to="/admin/dashboard" />
-//   </Switch>
-// );
+const switchRoutes = (
+  <Switch>
+    {routes.map((prop, key) => {
+      if (prop.layout === "/admin"){
+        return (
+          <Route
+            path={prop.layout + prop.path}
+            component={prop.component}
+            key={key}
+          />
+        );
+      }
+      return null;
+    })}
+    <Redirect from="/admin" to="/admin/dashboard" />
+  </Switch>
+);
 
 const useStyles = makeStyles(styles);
 
-function Admin({ ...rest }) {
+export default function Admin({ ...rest }) {
   // styles
   const classes = useStyles();
   // ref to help us initialize PerfectScrollbar on windows devices
@@ -66,9 +63,9 @@ function Admin({ ...rest }) {
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
   };
-  // const getRoute = () => {
-  //   return window.location.pathname !== "/admin/maps";
-  // };
+  const getRoute = () => {
+    return window.location.pathname !== "/admin/maps";
+  };
   const resizeFunction = () => {
     if (window.innerWidth >= 960) {
       setMobileOpen(false);
@@ -111,14 +108,14 @@ function Admin({ ...rest }) {
           {...rest}
         />
         {/* On the /maps route we want the map to be on full screen - this is not possible if the content and conatiner classes are present because they have some paddings which would make the map smaller */}
-        {/* {getRoute() ? (
+        {getRoute() ? (
           <div className={classes.content}>
             <div className={classes.container}>{switchRoutes}</div>
           </div>
         ) : (
           <div className={classes.map}>{switchRoutes}</div>
-        )} */}
-        {/* {getRoute() ? <Footer /> : null} */}
+        )}
+        {getRoute() ? <Footer /> : null}
         <FixedPlugin
           handleImageClick={handleImageClick}
           handleColorClick={handleColorClick}
@@ -131,5 +128,3 @@ function Admin({ ...rest }) {
     </div>
   );
 }
-
-export default Admin;
